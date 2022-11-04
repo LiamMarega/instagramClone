b0e1ad7164a036ac00dfa59607d1f1d6f88b075e
LiamMarega <liammarega85@gmail.com>
2022-11-04 11:24:33 -0300

    User models and Bottom navigator

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,11 +2,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/models/user.dart' as model;
 import 'package:instagram_flutter/resources/storage_methods.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<model.User> getUserDetails() async {
+    User currentUser = _auth.currentUser!;
+
+    DocumentSnapshot snap =
+        await _firestore.collection('users').doc(currentUser.uid).get();
+
+    return model.User.fromSnap(snap);
+  }
+
   //sign up user
   Future<String> signUpUser({
     required String email,
@@ -15,12 +26,13 @@ class AuthMethods {
     required String bio,
     required Uint8List file,
   }) async {
-    String res = "Some error occurred while signing up";
+    String res = "Some error occurred";
     try {
       if (email.isNotEmpty ||
           password.isNotEmpty ||
           username.isNotEmpty ||
-          bio.isNotEmpty) {
+          bio.isNotEmpty ||
+          file != null) {
         //register user
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
@@ -30,36 +42,26 @@ class AuthMethods {
 
         //add user to our database
 
-        await _firestore.collection('users').doc(cred.user!.uid).set({
-          'username': username,
-          'uid': cred.user!.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'photoUrl': photoUrl,
-        });
+        model.User _user = model.User(
+          username: username,
+          uid: cred.user!.uid,
+          email: email,
+          bio: bio,
+          photoUrl: photoUrl,
+          following: [],
+          followers: [],
+        );
+        await _firestore
+            .collection("users")
+            .doc(cred.user!.uid)
+            .set(_user.toJson());
 
-        // await _firestore.collection('users').add({
-        //   'username': username,
-        //   'uid': cred.user!.uid,
-        //   'email': email,
-        //   'bio': bio,
-        //   'followers': [],
-        //   'following': [],
-        // });
-
-        res = 'success';
+        res = "success";
+      } else {
+        res = "Please enter all the fields";
       }
-    } on FirebaseAuthException catch (err) {
-      res = err.message.toString();
-
-      if (err.code == 'wek-password') res = 'The password is invalid';
-      if (err.code == 'invalid-username') res = 'The username is invalid';
-      if (err.code == 'invalid-email') res = 'The email address is invalid';
-      if (err.code == 'invalid-bio') res = 'The bio is invalid';
     } catch (err) {
-      res = err.toString();
+      return err.toString();
     }
     return res;
   }
